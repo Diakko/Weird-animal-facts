7e74cf878a9e07d84bb96f2a70a98fee48df9061
Diakko <48632853+Diakko@users.noreply.github.com>
2020-04-27 15:18:10 +0300

    added database and models

---
+++ models/pictureModel.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const getPicture = async (id) => {
+    try {
+        const [rows] = await promisePool.query('SELECT * FROM wop_picture WHERE picture_id = ?', [ id ]);
+        return rows[0];
+    } catch (e) {
+        console.error('error', e.message);
+    }
+};
+
+module.exports = {
+    getPicture,
+}
---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const ul = document.querySelector('ul');
+const createPictureCards = (pictures) => {
+    // Clear ul
+    ul.innerHTML = '';
+    pictures.forEach((picture) => {
+        const img = document.createElement('img');
+
+        });
+};
