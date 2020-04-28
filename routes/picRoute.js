02878becd9f8aff0e04043d25d7fe28ab45f3cd1
Diakko <48632853+Diakko@users.noreply.github.com>
2020-04-28 13:07:05 +0300

    added picroute like week 3 catroute

---
+++ routes/picRoute.js
@@ -0,0 +1,62 @@
+'use strict';
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllPics = async () => {
+    try {
+        const [rows] = await promisePool.query('SELECT * FROM wop_pictures');
+        return rows;
+    } catch (e) {
+        console.error('error', e.message);
+    }
+};
+
+const getPic = async (id) => {
+    try {
+        const [rows] = await promisePool.query('SELECT * FROM wop_pictures WHERE pic_id = ?', [ id ]);
+        return rows[0];
+    }catch (e) {
+        console.error('error', e.message);
+    }
+};
+
+const insertPic = async (pic) => {
+    try {
+        console.log('insert pic?', pic);
+        const [rows] = await promisePool.query('INSERT INTO wop_pictures (title, description, filename) VALUES (?, ?, ?)',
+            [ pic.title, pic.description, pic.filename ]);
+        return rows;
+    } catch (e){
+        console.error('error', e.message);
+    }
+};
+const updatePic = async (pic) => {
+    try {
+        console.log('insert pic?', pic);
+        const [rows] = await promisePool.query('UPDATE wop_pic SET title = ?, description = ? WHERE wop_pic.pic_id = ?',
+            [ pic.title, pic.description, pic.id ]);
+        return rows;
+    } catch (e) {
+        console.error('updateCat model crash', e.message);
+    }
+};
+
+const deletePic = async (id) => {
+    try {
+        console.log('delete pic', id);
+        const [rows] = await promisePool.query('DELETE FROM wop_pic WHERE wop_pic.pic_id = ?', [ id ]);
+        console.log('deleted?', rows);
+        return rows;
+    } catch (e) {
+        console.error('deletePic model', e.message);
+    }
+};
+
+module.exports = {
+    getAllPics,
+    getPic,
+    insertPic,
+    deletePic,
+    updatePic,
+
+};
