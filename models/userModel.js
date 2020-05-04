5754b1989c022549d9c3233fad75bf7f9ba45d28
Diakko <48632853+Diakko@users.noreply.github.com>
2020-05-04 15:18:20 +0300

    styling by Dofi, added authcontroller and right databasename for usermodel

--- models/userModel.js
+++ models/userModel.js
@@ -17,7 +17,7 @@ const getAllUsers = async () => {
 
 const getUser = async (id) => {
   try {
-    const [rows] = await promisePool.query('SELECT user_id, name, email FROM wop_user WHERE user_id = ?', [ id ]);
+    const [rows] = await promisePool.query('SELECT user_id, name, email FROM wop_users WHERE user_id = ?', [ id ]);
     return rows[0];
   } catch (e) {
     console.error('error', e.message);
@@ -27,7 +27,7 @@ const getUser = async (id) => {
 const insertUser = async (user) => {
   try {
     console.log('insert user?', user);
-    const [rows] = await promisePool.query('INSERT INTO wop_user (name, email, password) VALUES (?, ?, ?)', [ user.name, user.email, user.passwd ]);
+    const [rows] = await promisePool.query('INSERT INTO wop_users (name, email, password) VALUES (?, ?, ?)', [ user.name, user.email, user.passwd ]);
     return rows;
   } catch (e) {
     console.error('error', e.message);
@@ -37,7 +37,7 @@ const insertUser = async (user) => {
 const updateUser = async (user) => {
   try {
     console.log('insert user?', user);
-    const [rows] = await promisePool.query('UPDATE wop_user SET name = ?, email = ?, password = ? WHERE wop_user.user_id = ?', [ user.name, user.email, user.passwd, user.id ]);
+    const [rows] = await promisePool.query('UPDATE wop_users SET name = ?, email = ?, password = ? WHERE wop_users.user_id = ?', [ user.name, user.email, user.passwd, user.id ]);
     return rows;
   } catch (e) {
     console.error('updateUser model crash', e.message);
@@ -47,13 +47,24 @@ const updateUser = async (user) => {
 const deleteUser = async (id) => {
   try {
     console.log('delete user', id);
-    const [rows] = await promisePool.query('DELETE FROM wop_user WHERE wop_user.user_id = ?', [ id ]);
+    const [rows] = await promisePool.query('DELETE FROM wop_users WHERE wop_user.user_id = ?', [ id ]);
     console.log('deleted?', rows);
     return rows;
   } catch (e) {
     console.error('deleteUser model', e.message);
   }
-}
+};
+const getUserLogin = async (params) => {
+  try {
+    console.log(params);
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_users WHERE email = ?;',
+        params);
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
 
 module.exports = {
   getAllUsers,
@@ -61,4 +72,5 @@ module.exports = {
   insertUser,
   updateUser,
   deleteUser,
+  getUserLogin,
 };
